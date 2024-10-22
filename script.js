e86da65b2f40b2ad9b2cb51ea420574f4a199e4d
SudoSuMaster <l.moermond21@gmail.com>
2024-10-22 18:16:57 +0200

    Je commit boodschap hier

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// script.js
-
-// Selecteer de elementen
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
 
 menuToggle.addEventListener('click', () => {
-    // Toggle de class 'active' om het menu te tonen/verbergen
     nav.classList.toggle('active');
 });
